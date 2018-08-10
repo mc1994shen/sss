41de6aae8c02753e7477af7086e6c2290dae5901
mc1994shen <421429437@qq.com>
2018-08-10 09:39:59 +0800

    task1-4

--- JS/task1/js/task1.js
+++ JS/task1/js/task1.js
@@ -1,7 +1,6 @@
 
-
+//洗牌
 function Shuffle(a) {
-    
     var length = a.length;
     for(var i = length - 1, rand;i >= 0; i--){
         rand = Math.floor(Math.random()*(i+1))
@@ -10,8 +9,11 @@ function Shuffle(a) {
         a[rand] = p ;  
     }
 }
+//设置从0到8的数组a
 a = [0,1,2,3,4,5,6,7,8];
+//页面加载运行函数，Shuffle（a）
 Shuffle(a);
+//随机抽取颜色
 function randomlyColor(){
     var red,blue,green;
     red = Math.floor(Math.random() * 256);
@@ -23,6 +25,7 @@ function randomlyColor(){
     }while(blue == red || blue == green);
     return("rgb(" + red + "," + green + "," + blue + ")");
 }
+//重置颜色，并随机抽取三个数，赋予其颜色
 function clear(){
     var units = document.getElementsByClassName("grid");
     for(var i = 0; i < units.length;i++){
@@ -33,11 +36,13 @@ function clear(){
     units[a.slice(0,3)[2]].style.backgroundColor = randomlyColor();
     console.log(a.slice(0,3)[0],a.slice(0,3)[1],a.slice(0,3)[2])
 }
+//设置定时运行函数Shuffle(a)和clear()
 function Timer() {
     Shuffle(a);
     clear();
     t = setTimeout("Timer()",1000);
 }
+//开始闪
 function start() {
     if (status == 0){
         Timer();
@@ -46,6 +51,7 @@ function start() {
         return;
     }
 }
+//结束闪
 function end() {
     clearTimeout(t);
     status = 0;
