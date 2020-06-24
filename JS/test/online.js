e55b8db04da2cffa85fb0bdaef55304bf5c68e29
mc1994shen <421429437@qq.com>
2020-06-24 21:33:26 +0800

    test

--- JS/test/online.js
+++ JS/test/online.js
@@ -1,3 +1,17 @@
 window.addEventListener('offline', ()=>{
     document.querySelector('div').innerHTML("断网了")
 })
+
+
+window.addEventListener('load', function() {
+    var div = document.querySelector("div");
+  
+    function updateOnlineStatus(event) {
+      var condition = navigator.onLine ? "online" : "offline";
+  
+      div.innerHTML = condition.toUpperCase();
+    }
+  
+    window.addEventListener('online',  updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+  });
